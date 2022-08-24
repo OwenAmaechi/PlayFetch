757c03f39094d391d0ec3aafbae5e737469743ee
Owen_A <chinazo.owen@gmail.com>
2022-08-24 15:41:11 +0100

    finalized
    the challenge is complete

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -11,7 +11,7 @@ public class PlayerControllerX : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    // On spacebar press, send dog
+    // On spacebar press, send dog.
     if (Input.GetKeyDown(KeyCode.Space) && pause <= 0)
     {
       Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
